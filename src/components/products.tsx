c5bbf9f05197a6f0f9c7699263fff88104123e17
RianLimeira <rianlimeira@gmail.com>
2024-02-08 15:55:57 -0300

    Mostrando produtos e listando

---
+++ src/components/products.tsx
@@ -0,0 +1,24 @@
+import { Image, ImageProps, Text, TouchableOpacity, TouchableOpacityProps, View } from "react-native";
+
+
+type ProductsDataProps = {
+    title: string
+    description: string
+    thumbnail: ImageProps
+}
+
+type ProductsProps = TouchableOpacityProps & {
+    data: ProductsDataProps
+}
+
+export function Products({ data, ...rest }: ProductsProps) {
+    return(
+        <TouchableOpacity className="w-full flex-row items-center pb-4" {...rest}>
+            <Image source={data.thumbnail} className="w-20 h-20 rounded-md" />
+            <View className="flex-1 ml-3">
+                <Text className="text-slate-100 font-subtitle text-base flex-1">{data.title}</Text>
+                <Text className="text-slate-400 text-xs leading-5 mt-0.5">{data.description}</Text>
+            </View>
+        </TouchableOpacity>
+    )
+}
